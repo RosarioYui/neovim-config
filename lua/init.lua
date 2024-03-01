747c6eaf1e80c6defc46313c0286abae6ca82403
RunjiLiu <liurunji666@gmail.com>
2024-03-01 17:17:45 +0800

    update some plugin load stragety

--- lua/init.lua
+++ lua/init.lua
@@ -10,6 +10,22 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
 
+opts = {
+    performance = {
+        cache = {
+        enabled = true,
+        },
+        reset_packpath = true, -- reset the package path to improve startup time
+        rtp = {
+            reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+            ---@type string[]
+            paths = {}, -- add any custom paths here that you want to includes in the rtp
+            ---@type string[] list any plugins you want to disable here
+            disabled_plugins = {
+            },
+        },
+    }
+}
 
+require("lazy").setup("plugins", opts)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,47 +1,78 @@
 return {
-    {"nvim-tree/nvim-web-devicons", lazy=true},
-    {"neovim/nvim-lspconfig", lazy=true, dependencies = {
-        {"williamboman/mason.nvim", lazy=true},
-        {"williamboman/mason-lspconfig.nvim", lazy=true}
-    }, config = function()
-        require("plugin_configs.lsp")
-    end
+    {
+        "nvim-tree/nvim-web-devicons", 
+        lazy=true
+    },
+    {
+        "neovim/nvim-lspconfig", 
+        lazy=true, 
+        dependencies = {
+            {"williamboman/mason.nvim", lazy=true},
+            {"williamboman/mason-lspconfig.nvim", lazy=true}
+        }, 
+        config = function()
+            require("plugin_configs.lsp")
+        end
     },
     -- auto complete
-    {"hrsh7th/nvim-cmp", event = {"BufReadPre", "CmdlineEnter"}, dependencies = {
-        {"hrsh7th/cmp-nvim-lsp"},
-        {"hrsh7th/cmp-buffer"},
-        {"hrsh7th/cmp-path"},
-        {"hrsh7th/cmp-cmdline"},
-        {"quangnguyen30192/cmp-nvim-tags"},
-        {"saadparwaiz1/cmp_luasnip"},
-    }, config = function()
-        require("plugin_configs.cmp")
-    end},
-    {"github/copilot.vim",lazy=tru},
+    -- event = {"BufReadPre", "CmdlineEnter", 
     {
-    'stevearc/aerial.nvim',
-    opts = {},
-    -- Optional dependencies
-    dependencies = {
-     "nvim-treesitter/nvim-treesitter",
-     "nvim-tree/nvim-web-devicons"
-    }, lazy=true
+        "hrsh7th/nvim-cmp", 
+        event = "InsertEnter", 
+        dependencies = {
+            {"hrsh7th/cmp-nvim-lsp"},
+            {"hrsh7th/cmp-buffer"},
+            {"hrsh7th/cmp-path"},
+            {"hrsh7th/cmp-cmdline"},
+            {"quangnguyen30192/cmp-nvim-tags"},
+            {"saadparwaiz1/cmp_luasnip"},
+        },
     },
+    {
+        "github/copilot.vim",
+        lazy=true
+    },
+    {
+        'stevearc/aerial.nvim',
+        dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+        }, lazy=true
+    },
+
     -- rust
-    {"simrat39/rust-tools.nvim", dependencies = {
-        {"neovim/nvim-lspconfig", lazy=true}    
-    }},
-    -- clangd
-    {"p00f/clangd_extensions.nvim", dependencies = {
-        {"neovim/nvim-lspconfig", lazy=true}    
-    }},
-    -- colorsheme
-    {"AlexvZyl/nordic.nvim",lazy=true},
-    {"rafamadriz/neon",lazy=true},
-    {"rmehri01/onenord.nvim",lazy=true},
     {
-    'nvim-lualine/lualine.nvim',dependencies = { 'nvim-tree/nvim-web-devicons'},lazy=true
+        "simrat39/rust-tools.nvim", 
+        dependencies = {
+        {"neovim/nvim-lspconfig", lazy=true}    
+        }, lazy=true
     },
-    {'akinsho/bufferline.nvim',lazy=true} 
+
+    -- clangd
+    {
+        "p00f/clangd_extensions.nvim", 
+        dependencies = {
+        {"neovim/nvim-lspconfig", lazy=true}    
+        }, lazy=true
+    },
+
+    -- colorsheme
+    -- {"AlexvZyl/nordic.nvim",lazy=true},
+    -- {"rafamadriz/neon",lazy=true},
+    {
+        "rmehri01/onenord.nvim",
+        lazy=false,
+        priority=1000, 
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons'},lazy=true,
+        "rmehri01/onenord.nvim",
+        lazy=false,
+        priority=1000,
+    },
+    {
+        'akinsho/bufferline.nvim',
+        lazy=true
+    } 
 }
