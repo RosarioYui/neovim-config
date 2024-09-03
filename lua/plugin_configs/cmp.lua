c24e7bc88cc34dc887928c7b6805d4f12f57d483
lrj <liurunji666@gmail.com>
2024-09-03 12:14:51 +0800

    dump

--- lua/plugin_configs/cmp.lua
+++ lua/plugin_configs/cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require('cmp')
 --local luasnip = require('luasnip')
 
@@ -15,13 +21,38 @@ cmp.setup{
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
         ['<C-j>'] = cmp.mapping.select_next_item(),
         ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<Up>'] = cmp.mapping.select_prev_item(),
         ['<Down>'] = cmp.mapping.select_next_item(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true, behavior=cmp.ConfirmBehavior.Insert })
+        ['<CR>'] = cmp.mapping.confirm({ select = true, behavior=cmp.ConfirmBehavior.Insert }),
+        -- Use <C-b/f> to scroll the docs
+        ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- Use <CR>(Enter) to confirm selection
+        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- A super tab
+        -- sourc: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            local copilot_keys = vim.fn['copilot#Accept']()
+            -- Hint: if the completion menu is visible select next one
+            if cmp.visible() then
+                cmp.select_next_item()
+--            elseif copilot_keys ~= '' and type(copilot_keys) == 'string' then
+--                vim.api.nvim_feedkeys(copilot_keys, 'i', true)
+--            elseif has_words_before() then
+--                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }), -- i - insert mode; s - select mode
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     }),
     sources = cmp.config.sources({
         {name = 'nvim_lsp'},
@@ -31,7 +62,13 @@ cmp.setup{
     }),
     formatting = {
         format = function(entry, vim_item)
---            vim_item.abbr = string.sub(vim_item.abbr, 1, 30)
+            vim_item.abbr = string.sub(vim_item.abbr, 1, 30)
+            vim_item.menu = ({
+              nvim_lsp = '[Lsp]',
+              luasnip = '[Luasnip]',
+              buffer = '[File]',
+              path = '[Path]',
+          })[entry.source.name]
             return vim_item
         end
     }
